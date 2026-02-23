71345f58887c53884c7a9429cab0efbeb478ac46
kiranmondal1980 <118626526+kiranmondal1980@users.noreply.github.com>
2026-02-23 08:35:59 +0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,4 @@
+
+    self.addEventListener('install', (e) => { e.waitUntil(caches.open('titan-store').then((cache) => cache.addAll(['./index.html']))); });
+    self.addEventListener('fetch', (e) => { e.respondWith(caches.match(e.request).then((response) => response || fetch(e.request))); });
+    
